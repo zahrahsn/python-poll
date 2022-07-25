d62d00201f5d941caa0d8fa2ac0422c71e820154
zahrahsn <4z.hosseinzade@gmail.com>
2022-07-25 15:13:56 +0200

    initial

---
+++ main.py
@@ -0,0 +1,52 @@
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app= FastAPI()
+"""
+usernme
+email
+created_at
+updated_at
+"""
+class User(BaseModel):
+    username:str
+    email:str
+    # created_at:datetime.datetime
+    # updated_at:datetime.datetime
+
+
+
+
+class Poll(BaseModel):
+    title:str
+    type:str
+    is_add_choices_active:bool
+    is_voting_active:bool
+    created_by:int
+    # created_at:datetime.datetime
+    # updated_at:datetime.datetime
+
+
+@app.get("/")
+async def root():
+    return {"message":"Hello Zahra!"}
+
+@app.get("/polls")
+async def root():
+    return {"polls":"Hello Zahra!"}
+
+
+@app.get("/users")
+async def root():
+    return {"users":"Hello Zahra!"}
+
+
+
+@app.post("/users")
+async def create_user(user:User):
+    return user
+
+@app.post("/polls")
+async def create_poll(poll:Poll):
+    return poll
